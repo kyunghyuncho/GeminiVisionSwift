b6602922666171b9b1340ac7896f9a3afdcc40b6
Kyunghyun Cho <kyunghyun.cho@nyu.edu>
2025-07-06 11:41:05 -0400

    Initial Commit

---
+++ GeminiVisionSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeminiVisionSwift
+//
+//  Created by KyungHyun Cho on 7/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeminiVisionSwift/GeminiVisionSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiVisionSwiftApp.swift
+//  GeminiVisionSwift
+//
+//  Created by KyungHyun Cho on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiVisionSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
